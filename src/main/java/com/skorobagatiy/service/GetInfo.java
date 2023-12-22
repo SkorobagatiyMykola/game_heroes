287363a8cc7d2b78f7a80bcaa40f4846c17630a6
SkorobagatiyMykola <nick1980457@gmail.com>
2023-12-22 21:46:00 +0200

    Initial commit

---
+++ src/main/java/com/skorobagatiy/service/GetInfo.java
@@ -0,0 +1,10 @@
+package com.skorobagatiy.service;
+
+public class GetInfo {
+    private static String url = "https://www.heroeswm.ru/map.php";
+
+    public void getGoldInfo(){
+        
+    }
+
+}
