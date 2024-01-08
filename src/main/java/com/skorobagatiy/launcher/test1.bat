552ab76a44ceb2543a70380aee6d05ff81ab5a11
SkorobagatiyMykola <nick1980457@gmail.com>
2024-01-08 16:40:20 +0200

    Added two files for launcher

---
+++ src/main/java/com/skorobagatiy/launcher/test1.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Hello world, My test !!
+timeout 1
+:: java Main
+timeout 1
+java -jar game_heroes.jar
+timeout 3671
+java -jar game_heroes.jar
+timeout 3682
+java -jar game_heroes.jar
+timeout 3683
+java -jar game_heroes.jar
+timeout 3694
+java -jar game_heroes.jar
